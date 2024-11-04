3a52bfdff73948f78e703a77ffdda22a3fa8b97d
rahul-kalwar <rahulkumarkalwar40@gmail.com>
2024-11-04 21:17:27 +0200

    todo_4 completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+const url = 'http://localhost:3001'
 
 function App() {
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
 
-  const addTask = () => {
-    if (task.trim()) {
-      
-      setTasks([...tasks, task]);
-      setTask(''); 
-    }
-  };
+  useEffect(() => {
+    axios.get(url)
+      .then(response => {
+        setTasks(response.data)
+      }).catch(error => {
+        alert(error.response.data.error ? error.response.data.error : error)
+      })
+  }, [])
 
-  const deleteTask = (deleted) => {
-    const withoutRemoved = tasks.filter((item) => item !== deleted);
-    setTasks(withoutRemoved);
-  };
+  const addTask = () => {
+    axios.post(url + '/create', {
+      description: task
+    })
+
+      .then(response => {
+        setTasks([...tasks, { id: response.data.id, description: task }])
+        setTask('')
+      }).catch(error => {
+        alert(error.response.data.error || error.message)
+      })
+
+  }
+
+
+  const deleteTask = (id) => {
+    axios.delete(url + '/delete/' + id)
+      .then(response => {
+        const withoutRemoved = tasks.filter((item) => item.id !== id)
+        setTasks(withoutRemoved)
+      }).catch(error => {
+        alert(error.response.data.error ? error.response.data.error : error)
+      })
+  }
 
   return (
     <div id="container">
@@ -35,14 +58,10 @@ function App() {
         />
       </form>
       <ul>
-        {tasks.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button className="delete-button" onClick={() => deleteTask(item)}>
-              Delete
-            </button>
-          </li>
-        ))}
+        {tasks.map(item => (
+         <Row key= {item.id} item={item} deleteTask={deleteTask}/> 
+        ))
+      }    
       </ul>
     </div>
   );
